943865336768c86ed84d41cb26eca7e133bf8422
Adele-Nedzinskaite <adele.nedzinskaite@mif.stud.vu.lt>
2024-09-25 09:35:55 +0300

    Adding grammar

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,4 +5,6 @@ module Lib1
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+completions = ["upper" , "lower" , "letter", "digit", "number", "alphanumeric", "char", "string",
+"concert_tickets_seller", "concert_list", "concert", "title", "artist", "year", "tickets_list",
+"ticket", "ID", "type", "availability", "price"]
